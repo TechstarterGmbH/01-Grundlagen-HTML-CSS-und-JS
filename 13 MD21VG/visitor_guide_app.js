4a3cdd3438d042afe69a43d884c9e59cb8ce1470
Manuel Fischer <mail@manuelfischer.de>
2022-12-15 11:22:06 +0100

    Commit nach Rebase in Teamretro

--- 13 MD21VG/visitor_guide_app.js
+++ 13 MD21VG/visitor_guide_app.js
@@ -1,6 +1,5 @@
 import { Objekt21, machwas } from './modules/Objekt21.js';
 import { logger21 } from './modules/logger.js';
-//import('./modules/Object21.js');
 
 // Test Stephan
 const C = console.log.bind(console);
@@ -9,24 +8,31 @@ const C = console.log.bind(console);
 //Beispiel für den import einer Funktion
 machwas()
 
-// let o21 = new Objekt21();
-// console.log (o21.objektID)
-// logger21.log("meine Lognachricht")
 
-//Der Visitor Guide beseteht derzeit aus einem einzelnen Map-Pbjekt
+//Der Visitor Guide beseteht derzeit aus einem einzelnen Map-Objekt
 let vg = new Map()
+
+/**
+ * Vorgehen
+ */
+// 1. Daten müssen in den Visitor Guide geladen werden
+// 2. Daten müssen (vor-) verarbeitet werden
+// 3. Website muss erstellt
+// 4. Daten auf der Website darstellen
+
+
 
 /**
  * Start: Daten aus JSON Datei einlesen
  */
 
 // Variante 1: Mit Arrow Schreibweise, kuerzer
-// fetch("./data/objekt21-data.json")
+//fetch("./data/objekt21-data.json")
 //     .then((response) => response.json())
 //     .then((data) => console.log("FETCH Daten mit Arrowschreibweise: " + data.name));
 
 
-// Variante 2, Ohne Arrow Schreibweise, ausfuehrlicher
+// Variante 2, Ohne Arrow Schreibweise, ausfuhrlicher
 let responseFunc = function (response) {
     return response.json()
 }
@@ -47,9 +53,6 @@ let dataFunc = function (data){
     console.log(vg)
     console.log(vg.size)
 
-
-
-
     // let o21 = new Objekt21(data[0])
     // logger21.log(o21.objektID)
 
@@ -59,23 +62,33 @@ let dataFunc = function (data){
     // console.log("data: " + typeof(data))
     // console.log("o21: " + typeof(o21))
     
-    
     // o21.getAehnlicheObjekte();
-    
-
 }
 
 fetch("./data/objekt21-data.json").then(responseFunc).then(dataFunc);
-   
 
 
-
-// 1. Daten müssen in den Visitor Guide
 vg.set("O23KE", new Objekt21())
 console.log ( "MAP" + vg.get("O23KE").name)
 
-// 2. Daten müssen (vor-) verarbeitet werden
-// 3. Website muss erstellt
-// 4. Daten auf der Website darstellen
 
+<<<<<<< Updated upstream
 //logger21.log("check");
+=======
+
+
+
+// Variante 3: Fetch Aufruf mit await
+const fetchRueckgabe = await fetch("./data/objekt21-data.json")
+console.log ("status " + fetchRueckgabe.status)
+console.log ("status Text " + fetchRueckgabe.statusText)
+console.log ("type" + fetchRueckgabe.type)
+console.log (fetchRueckgabe.headers)
+console.log (fetchRueckgabe.url)
+
+let museumsDatenAlsJson = await fetchRueckgabe.json();
+console.log ("json: " + museumsDatenAlsJson[0].objektID)
+
+let o21 = new Objekt21(museumsDatenAlsJson[0])
+console.log(" Fetch mit Awsait: "  + o21.objektID);
+>>>>>>> Stashed changes
