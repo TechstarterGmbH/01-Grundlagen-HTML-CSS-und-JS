d5fa80c748a3ce0f884afb3f0ea36e803ec99f10
Manuel Fischer <mail@manuelfischer.de>
2022-12-15 15:41:35 +0100

    Clean-up

--- 13 MD21VG/visitor_guide_app.js
+++ 13 MD21VG/visitor_guide_app.js
@@ -23,51 +23,8 @@ let vg = new Map()
 
 
 /**
- * Start: Daten aus JSON Datei einlesen
+ * Start:   Daten aus JSON Datei einlesen und Liste von Objekten 
+ *          (in Form einer Map) anlegen
  */
-
-// Variante 3: Fetch Aufruf mit await
-// const fetchRueckgabe = await fetch("./data/objekt21-data.json")
-// let o21_json = await fetchRueckgabe.json();
-// for (let index = 0; index < o21_json.length; index++){
-//     let o = new Objekt21(o21_json[index], Objekt21.Option.SINGLE_OBJEKT21)
-//     vg.set(o.objektID,o);
-//     logger21.log(o.objektID)
-// }
-
 const o21Map = await Objekt21.CreateO21Map("./data/objekt21-data.json")
 console.log("Anzahl: " + o21Map.size);
-
-
-
-
-// let o21 = new Objekt21(o21_json[0])
-// console.log(" Fetch mit Awsait: "  + o21.objektID);
-
-// // Variante 2, Ohne Arrow Schreibweise, ausfuhrlicher
-// let responseFunc = function (response) {
-//     return response.json()
-// }
-
-// let dataFunc = function (data){
-
-//     for (let index = 0; index < data.length; index++){
-//         let o = new Objekt21(data[index])
-//         vg.set(o.objektID,o);
-//         logger21.log(o.objektID)
-//     }
-//     console.log(vg)
-//     console.log(vg.size)
-// }
-
-// fetch("./data/objekt21-data.json").then(responseFunc).then(dataFunc);
-
-
-
-// console.log ( "MAP" + vg.get("O23KE").name)
-
-
-
-
-
-
