3d1261b16721007abeab3edf8a2a0e50962cb64f
Manuel Fischer <mail@manuelfischer.de>
2022-12-15 15:38:53 +0100

    Fetching in O21-KLasse verlagert

--- 13 MD21VG/visitor_guide_app.js
+++ 13 MD21VG/visitor_guide_app.js
@@ -26,69 +26,48 @@ let vg = new Map()
  * Start: Daten aus JSON Datei einlesen
  */
 
-// Variante 1: Mit Arrow Schreibweise, kuerzer
-//fetch("./data/objekt21-data.json")
-//     .then((response) => response.json())
-//     .then((data) => console.log("FETCH Daten mit Arrowschreibweise: " + data.name));
-
-
-// Variante 2, Ohne Arrow Schreibweise, ausfuhrlicher
-let responseFunc = function (response) {
-    return response.json()
-}
-
-let dataFunc = function (data){
-    // console.log("FETCH Daten mit Callback Funktionen: " + data.name)
-    // logger21.log(data.objektID)
-    // logger21.log(data.name)
-    // logger21.log(data.langbeschreibung)
-    // logger21.log(data.urheber)
-
-
-    for (let index = 0; index < data.length; index++){
-        let o = new Objekt21(data[index])
-        vg.set(o.objektID,o);
-        logger21.log(o.objektID)
-    }
-    console.log(vg)
-    console.log(vg.size)
-
-    // let o21 = new Objekt21(data[0])
-    // logger21.log(o21.objektID)
-
-    // o21 = new Objekt21(data[1])
-    // logger21.log(o21.objektID)
-
-    // console.log("data: " + typeof(data))
-    // console.log("o21: " + typeof(o21))
-    
-    // o21.getAehnlicheObjekte();
-}
-
-fetch("./data/objekt21-data.json").then(responseFunc).then(dataFunc);
-
-
-vg.set("O23KE", new Objekt21())
-console.log ( "MAP" + vg.get("O23KE").name)
-
-
-<<<<<<< Updated upstream
-//logger21.log("check");
-=======
-
-
-
 // Variante 3: Fetch Aufruf mit await
-const fetchRueckgabe = await fetch("./data/objekt21-data.json")
-console.log ("status " + fetchRueckgabe.status)
-console.log ("status Text " + fetchRueckgabe.statusText)
-console.log ("type" + fetchRueckgabe.type)
-console.log (fetchRueckgabe.headers)
-console.log (fetchRueckgabe.url)
+// const fetchRueckgabe = await fetch("./data/objekt21-data.json")
+// let o21_json = await fetchRueckgabe.json();
+// for (let index = 0; index < o21_json.length; index++){
+//     let o = new Objekt21(o21_json[index], Objekt21.Option.SINGLE_OBJEKT21)
+//     vg.set(o.objektID,o);
+//     logger21.log(o.objektID)
+// }
 
-let museumsDatenAlsJson = await fetchRueckgabe.json();
-console.log ("json: " + museumsDatenAlsJson[0].objektID)
+const o21Map = await Objekt21.CreateO21Map("./data/objekt21-data.json")
+console.log("Anzahl: " + o21Map.size);
 
-let o21 = new Objekt21(museumsDatenAlsJson[0])
-console.log(" Fetch mit Awsait: "  + o21.objektID);
->>>>>>> Stashed changes
+
+
+
+// let o21 = new Objekt21(o21_json[0])
+// console.log(" Fetch mit Awsait: "  + o21.objektID);
+
+// // Variante 2, Ohne Arrow Schreibweise, ausfuhrlicher
+// let responseFunc = function (response) {
+//     return response.json()
+// }
+
+// let dataFunc = function (data){
+
+//     for (let index = 0; index < data.length; index++){
+//         let o = new Objekt21(data[index])
+//         vg.set(o.objektID,o);
+//         logger21.log(o.objektID)
+//     }
+//     console.log(vg)
+//     console.log(vg.size)
+// }
+
+// fetch("./data/objekt21-data.json").then(responseFunc).then(dataFunc);
+
+
+
+// console.log ( "MAP" + vg.get("O23KE").name)
+
+
+
+
+
+
